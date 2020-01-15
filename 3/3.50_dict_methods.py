ec3de67edf9a9865c6351be882243a661819c5ee
Didi <ofisser86@gmail.com>
2020-01-15 18:51:34 +0200

    Add coments and exaple to dict method2 for better understanding

--- 3/3.50_dict_methods.py
+++ 3/3.50_dict_methods.py
@@ -3,9 +3,9 @@ user = {
     'store': 'phone',
     'age': 22
 }
-# try get name, if None get Didi as default
+# try get name, if None get Didi as default, don`t modify the dict
 print(user.get('name', 'Didi')) 
-
+print(user)
 # another way to create dict
 user2 = dict(name='John', age=33, surname='Doy')
 print(user2)
