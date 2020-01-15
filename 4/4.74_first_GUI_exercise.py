c3112b3037834eb36e7c8162514c60efc731decf
Didi <ofisser86@gmail.com>
2020-01-15 16:31:18 +0200

    Fundamental IV

--- 4/4.74_first_GUI_exercise.py
+++ 4/4.74_first_GUI_exercise.py
@@ -9,6 +9,18 @@ picture = [
   [0,0,0,1,0,0,0]
 ]
 # TODO: have to change variable i and j to row and pixel for better understanding
+# fill = '*'
+# empty = ''
+# for row in picture:
+#     for pixel in row:
+#         if pixel:
+#             print(fill, end='')
+#         else:
+#             print(empty, end='')
+#     print()
+
+
+
 for i in picture:
     for j in i:
         if j == 0:
