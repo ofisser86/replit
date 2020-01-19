a6d50cf7d9164d6ef4f58828631d1c54ee79d87c
Didi <ofisser86@gmail.com>
2020-01-19 19:53:43 +0200

    Create MyClass, __init__  methgod

---
+++ 6/6.110_OOP.py
@@ -0,0 +1,19 @@
+#OOP
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('hi'))
+print(type([]))
+print(type(()))
+print(type({}))
+class MyClass():
+    def __init__(self, name):
+        self.name = name
+
+
+
+man = MyClass("Didi")
+print(man.name)
+
+
