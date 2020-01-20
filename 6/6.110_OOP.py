e8191012f71b4fa172c372fe535117f6f87684bf
Didi <ofisser86@gmail.com>
2020-01-20 19:33:11 +0200

    Add test method

--- 6/6.110_OOP.py
+++ 6/6.110_OOP.py
@@ -8,12 +8,19 @@ print(type([]))
 print(type(()))
 print(type({}))
 class MyClass():
+    surname = ''
     def __init__(self, name):
         self.name = name
+    
+    def my_age(self, age):
+        my_age = age + 10
+        return my_age
 
 
 
 man = MyClass("Didi")
-print(man.name)
+age = man.my_age(23)
+
+print(man.name, age)
 
 
