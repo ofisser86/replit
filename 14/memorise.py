d7d31bf1cf2a89a094f574c7432f1d517192aa62
Didi <ofisser86@gmail.com>
2020-02-01 14:43:08 +0200

    try - exept - raise

--- 14/memorise.py
+++ 14/memorise.py
@@ -1,13 +1,16 @@
 def like_for(iterable):
     iterator = iter(iterable)
+    
+
+    
     while True:
         try:
-        
             print(iterator)
             print(next(iterator))
         except StopIteration:
              print('Some exception here')
              break
 
+my_list = [3, 1, 2, 6, 8, 7, 8, 9]
 
-print(like_for([3, 1, 2, 6, 8, 7, 8, 9]))
+print(like_for(my_list))
