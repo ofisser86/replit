00696834a8f6967daf42d6e1a87c1d6660cefaee
Didi <ofisser86@gmail.com>
2020-02-04 17:58:28 +0200

    Create JPG to PNG converter

--- 17/astro206.py
+++ 17/astro206.py
@@ -1,10 +1,24 @@
 from PIL import Image, ImageFilter
+from pathlib import Path
+import sys
 
 
-img = Image.open('/home/didi/dev/u2020/17/astro.jpg')
-converted_image = img.resize((400, 400))
-converted_image.show()
+img_path = Path(sys.argv[1])
+# files_in_basepath = basepath.iterdir()
+dest = Path(sys.argv[2])
+if not dest.exists():
+    dest.mkdir()
+
+img_list = img_path.glob('*.jpg')
+#print(len(list(img_list))
+for img in img_list:
+    image = Image.open(img)
+    resized_image = image.resize((400, 400))
+    new_file_name = str(img).strip('.jpg')
+    
+    resized_image.save(f'{sys.argv[2]}/{new_file_name}_resized.png', 'png')
+#resized_image.show()
 # for keep aspect ratio
-img.thumbnail((400, 400))
-img.save('thambnail.jpg')
-img.show()
+#img.thumbnail((400, 400))
+#img.save('thambnail.jpg')
+#img.show()
---
+++ 17/image-playground/JPGtoPNGconverter.py
@@ -0,0 +1,48 @@
+import os, sys
+from pathlib import Path
+from PIL import Image
+
+def right_slash(os_name):
+    good_slash = '/'
+    bad_slash = '\\'
+    if os_name != 'linux':
+        return bad_slash
+    return good_slash 
+
+try:
+    source_folder = sys.argv[1]
+    destination_folder = sys.argv[2]
+    slash = right_slash(sys.platform)
+    # check if slash exist but does not cover all cases  
+    if source_folder[-1] == slash and destination_folder[-1] == slash:
+        source_folder = source_folder.strip()
+        destination_folder = destination_folder.strip()
+    else:
+        source_folder += slash
+        destination_folder += slash 
+
+    # print(source_folder, destination_folder)
+    jpg_list = list(Path(source_folder).glob('*.jpg'))
+    
+    # Chech if folder is empty
+    if len(jpg_list) == 0:
+        raise FileNotFoundError('There are no jpg files. Try another folder')
+    
+
+
+    # Create Path obj and chech if directory exist, if not create new
+    dest_dir = Path(destination_folder)
+    if not dest_dir.exists():
+        dest_dir.mkdir() 
+
+    for img in jpg_list:
+        image = Image.open(img)
+        converted_img = image.convert("L")
+        
+        new_file_name = str(img).rstrip('.jpg').lstrip('Pokedex/')
+        converted_img.save(f'{dest_dir}/{new_file_name}_converted.png', 'png')
+
+    print("Converted!!!!!!!!!")
+except IndexError as err:
+    print(f'Add source and dest folders. Built in err is "{err}" ')
+
