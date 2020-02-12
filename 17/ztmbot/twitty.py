fef1677202f7a0b9d2e79ef6b1be0c6aa6edf355
Didi <ofisser86@gmail.com>
2020-02-12 18:41:59 +0100

    Init twitter bot

---
+++ 17/ztmbot/twitty.py
@@ -0,0 +1,19 @@
+import tweepy
+import bot_settings
+
+auth = tweepy.OAuthHandler(bot_settings.consumer_key, bot_settings.consumer_secret)
+auth.set_access_token(bot_settings.access_token, bot_settings.access_token_secret)
+
+api = tweepy.API(auth)
+
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
+
+user = api.me()
+print(user.name)
+print(user.followers_count)
+print(user.id)
+print(api.show_friendship(source_id='243369006', target_screen_name='kisuly808'))
+api.destroy_friendship(screen_name='kisuly808')
+print(api.show_friendship(source_id='243369006', target_screen_name='kisuly808')['following'])
