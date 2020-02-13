d025be4f06f57d8b82520f286a66e62f7778725b
Didi <ofisser86@gmail.com>
2020-02-13 22:23:15 +0100

    init more letters

---
+++ checkio/more_letters.py
@@ -0,0 +1,9 @@
+def checkio(text: str) -> str:
+    temp = [i for i in text]
+
+    #replace this for solution
+    return temp
+
+if __name__ == '__main__':
+    print("Example:")
+    print(checkio("Hello World!"))
