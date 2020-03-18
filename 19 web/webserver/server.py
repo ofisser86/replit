d58da071ae373b149cc0fb7e7afa02fa6a0e9dfa
Dm <ofisser86@gmail.com>
2020-03-18 21:05:02 +0100

    Add template and static files, init the app

--- 19 web/webserver/server.py
+++ 19 web/webserver/server.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/<username>/<int:post_id>')
+def hello_world(username, post_id):
+    return render_template('index.html', username='dmytro', post_id=3132416545)
 
 @app.route('/blog')
 def blog():
