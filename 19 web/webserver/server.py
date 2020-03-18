28c010c26e6e88e930865b1692cb74ca5b3591dc
Dm <ofisser86@gmail.com>
2020-03-18 17:05:39 +0100

    on debug mode with right command on PowerShell

--- 19 web/webserver/server.py
+++ 19 web/webserver/server.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
-FLASK_DEBUG=1
 print(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Ud!'
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return 'My blogpost!'
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    # in olser version use debug=true for debug mode on
+    app.run()
