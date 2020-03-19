cf26b93503859acba42bd0db30188dd3de5ddc79
Dm <ofisser86@gmail.com>
2020-03-19 20:34:50 +0100

    update templates

--- 19 web/webserver/server.py
+++ 19 web/webserver/server.py
@@ -11,25 +11,29 @@ def my_index():
 def blog():
     return 'My blogpost!'
 
-@app.route('/works')
-def works():
-    return render_template('works.html')
+@app.route('/<slug>')
+def works(slug):
+    return render_template(f'{slug}')
 
-@app.route('/work')
-def work():
-    return render_template('work.html')
+# @app.route('/works')
+# def works():
+#     return render_template('works.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+# @app.route('/work')
+# def work():
+#     return render_template('work.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
 
-@app.route('/components')
-def components():
-    return render_template('components.html')
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
+
+# @app.route('/components')
+# def components():
+#     return render_template('components.html')
 
 
 if __name__ == '__main__':
