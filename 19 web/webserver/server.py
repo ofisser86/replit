43463cc4a26669c881cf21c7b6c3390cb83fe6b9
Dm <ofisser86@gmail.com>
2020-03-19 13:57:39 +0100

    Add tenplate to web app

--- 19 web/webserver/server.py
+++ 19 web/webserver/server.py
@@ -3,17 +3,33 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username, post_id):
-    return render_template('index.html', username='dmytro', post_id=3132416545)
+@app.route('/')
+def my_index():
+    return render_template('index.html')
 
 @app.route('/blog')
 def blog():
     return 'My blogpost!'
 
+@app.route('/works')
+def works():
+    return render_template('works.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/components')
+def components():
+    return render_template('components.html')
 
 
 if __name__ == '__main__':
