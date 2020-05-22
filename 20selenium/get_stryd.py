1a1f9ef8f7f53ea206c7628f93c35a8406634af6
Didi <ofisser86@gmail.com>
2020-05-22 18:18:19 +0300

    Collect training data

--- 20selenium/get_stryd.py
+++ 20selenium/get_stryd.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(ABS_PATH)
 
 web_browser = webdriver.Firefox()
 # https://www.stryd.com/powercenterbeta/profile
-web_browser.get('https://www.stryd.com/powercenter/runs/5782253728366592')
+web_browser.get('https://www.stryd.com/powercenter')
 
 
 
@@ -26,11 +26,19 @@ web_browser.find_element_by_id('loginbutton').click()
 
 wait = WebDriverWait(web_browser, 10)
 element = wait.until(EC.title_contains('Stryd PowerCenter'))
+#web_browser.get(f'https://www.stryd.com/powercenter/calendar/?sid={web_browser.session_id}')
+
+
+myreaponse = web_browser.execute_script("return document.getElementById('5782253728366592')")
+print(myreaponse)
 
 web_browser.get(f'https://www.stryd.com/powercenter/runs/5782253728366592/?sid={web_browser.session_id}')
 
-wait.until(EC.title_contains('Very Easy Warmup'))
+wait.until(EC.text_to_be_present_in_element((By.TAG_NAME, 'h3'), 'Distance Splits'))
 
 el = web_browser.find_elements_by_tag_name('p')
-for items in el:
+for items in el[:31]:
     print(items.text)
+
+
+web_browser.quit()
